03e6dc4b5df882abf5b9492a869f6aafe9173684
David <david@osull.io>
2021-03-06 22:28:13 +0000

    dark mode dev

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,48 @@
-// Get full height
-
+// Get window.innerHeight
 function windowHeight() {
   const windowInnerHeight = window.innerHeight;
   const pageElement = document.querySelector('.page');
-  pageElement.style.height = `${windowInnerHeight}px`;
+  const mainElement = document.querySelector('main');
+  const mainStyle = mainElement.currentStyle || window.getComputedStyle(mainElement);
+  pageElement.style.height = `calc(${windowInnerHeight}px - (${mainStyle.paddingTop} + ${mainStyle.paddingBottom}}))`;
 }
+
 window.addEventListener('load', windowHeight);
 window.addEventListener('resize', windowHeight);
+
+// Dark Mode
+const darkModeInput = document.querySelector('.darkModeToggle');
+console.log(darkModeInput);
+
+function darkModeToggle() {
+  if (darkModeInput.checked === 'true') {
+    console.log('Checked');
+  } else {
+    console.log('Not Checked');
+  }
+}
+
+darkModeToggle();
+
+// const toggleColorMode = (el) => {
+//   if (el.currentTarget.classList.contains('light--hidden')) {
+//     document.documentElement.setAttribute('color-mode', 'light');
+//     localStorage.setItem('color-mode', 'light');
+//     return;
+//   }
+//   document.documentElement.setAttribute('color-mode', 'dark');
+//   localStorage.setItem('color-mode', 'dark');
+// };
+
+// const toggleColorButtons = document.querySelectorAll('.color-mode__btn');
+
+// toggleColorButtons.forEach((btn) => {
+//   btn.addEventListener('click', toggleColorMode);
+// });
+
+// if (
+//   localStorage.getItem('color-mode') === 'dark' ||
+//   (window.matchMedia('(prefers-color-scheme: dark)').matches && !localStorage.getItem('color-mode'))
+// ) {
+//   document.documentElement.setAttribute('color-mode', 'dark');
+// }
